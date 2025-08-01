3c9aed225f50d958265cf5b2b1fc401366689214
timothykusardi <timothykusardi@gmail.com>
2025-08-01 20:21:01 +0700

    adding head for logo

---
+++ src/app/head.tsx
@@ -0,0 +1,23 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>
+        Alpha Charcoal International | Premium Indonesian Charcoal &amp; Biomass
+      </title>
+      <meta
+        name="description"
+        content="Alpha Charcoal International: sustainably sourced charcoal & biomass from Kalimantan, Indonesia."
+      />
+      <meta
+        name="keywords"
+        content="arang kokas premium, ekspor arang Indonesia, arang kalimantan berkualitas, premium Indonesian charcoal, Kalimantan charcoal exporter, sustainable biomass supplier, فحم إندونيسي فاخر, تصدير فحم كاليمانتان"
+      />
+
+      {/* Favicon / tab icon */}
+      <link rel="icon" href="/alpha-logo.png" />
+      <link rel="shortcut icon" href="/alpha-logo.png" />
+      <link rel="apple-touch-icon" href="/alpha-logo.png" />
+    </>
+  );
+}
