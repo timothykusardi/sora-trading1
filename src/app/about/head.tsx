f79440aca9a07f28b7f92a60611cbf923c5b8774
timothykusardi <timothykusardi@gmail.com>
2025-08-06 18:31:10 +0700

    unzoomable

---
+++ src/app/about/head.tsx
@@ -0,0 +1,13 @@
+// src/app/about/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>About Us – Alpha Charcoal International</title>
+      {/* Lock zoom specifically on the About page */}
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+    </>
+  )
+}
